2d87043223ca62b05299ee0a8bc3e06f63b521a9
Austin Sherron <austin@pryvate.email>
2024-01-28 13:21:15 -0800

    [Feature] Add/Update Error Utils
    * Add ErrorMessage util class
    * Add Error.parse util that parses error messages
    * Add OnErr.getmsg util that returns a parsed error message if an error
      occurs
    * Remove luarc file

--- lua/toolbox/error/error.lua
+++ lua/toolbox/error/error.lua
@@ -1,3 +1,5 @@
+local Array = require 'toolbox.core.array'
+local Class = require 'toolbox.meta.class'
 local Stream = require 'toolbox.extensions.stream'
 local String = require 'toolbox.core.string'
 local Table = require 'toolbox.core.table'
@@ -7,6 +9,37 @@ local Table = require 'toolbox.core.table'
 ---@class Error
 local Error = {}
 
+--- Models an error message.
+---
+---@class ErrorMessage
+---@field msg string: an error message
+---@field code string|nil: an error code
+---
+---@field getmsg fun(): string|nil getter for msg
+---@field getcode fun(): string|nil getter for code
+
+--- Constructor
+---
+---@param msg string|nil: see class docs
+---@param code string|nil: see class docs
+---@return ErrorMessage: a new instance
+local ErrorMessage = Class(function(msg, code)
+  return { msg = msg, code = code }
+end)
+
+---@return boolean: true if the message has neither a msg nor code, false otherwise
+function ErrorMessage:empty()
+  return self.msg == nil and self.code == nil
+end
+
+---@return string: a string representation of this instance
+function ErrorMessage:__tostring()
+  return String.fmt('msg=%s, code=%s', self.msg or '?', self.code or '?')
+end
+
+---@note: so ErrorMessage is publicly exposed
+Error.ErrorMessage = ErrorMessage
+
 --- Constructs an error msg by stringifying args, formatting them into base_msg, and
 --- using it to raise an error.
 ---
@@ -15,8 +48,27 @@ local Error = {}
 ---@error an error w/ a message formatted from the provided string and tokens
 function Error.raise(base_msg, ...)
   local args = Stream.new(Table.pack(...)):map(String.tostring):collect()
-
   error(string.format(base_msg, Table.unpack(args)))
+end
+
+local function trim(str)
+  return String.trim(str)
+end
+
+--- Parses the error msg and returns its parts.
+---
+---@param msg string|nil: the error message to parse
+---@return ErrorMessage: the error message to parse
+function Error.parse(msg)
+  local parts = Stream.new(String.split(msg or '', ':', 1)):map(trim):collect()
+
+  if Array.len(parts) == 2 then
+    return ErrorMessage(parts[2], parts[1])
+  elseif Array.len(parts) == 1 then
+    return ErrorMessage(parts[1])
+  else
+    return ErrorMessage()
+  end
 end
 
 return Error
--- lua/toolbox/error/onerr.lua
+++ lua/toolbox/error/onerr.lua
@@ -1,3 +1,5 @@
+local Error = require 'toolbox.error.error'
+
 --- Contains utility methods that wrap function calls and perform specific actions when the
 --- functions raise errors.
 ---
@@ -6,10 +8,10 @@ local OnErr = {}
 
 --- On error, returns false.
 ---
----@param f fun(): any?: the function that might throw an error
----@param ... any?: args to pass to f
+---@param f fun(): any|nil: the function that might throw an error
+---@param ... any|nil: args to pass to f
 ---@return boolean: true if the function completes w/out error, false otherwise
----@return any?: the result of f, if any
+---@return any|nil: the result of f, if any
 function OnErr.as_bool(f, ...)
   return pcall(f, ...)
 end
@@ -19,7 +21,7 @@ end
 ---@generic T
 ---@param f fun(...): T|nil: the function that might throw an error
 ---@param sub Callable: the value to substitute on error
----@param ... any?: args to pass to f
+---@param ... any|nil: args to pass to f
 ---@return T: the return value of f, or the return value of sub, if f encounters errors
 ---@return string|nil: the response from any errors encountered calling f, if any
 function OnErr.substitute(f, sub, ...)
@@ -30,6 +32,20 @@ function OnErr.substitute(f, sub, ...)
   end
 
   return sub(), res
+end
+
+--- On error, returns the error's message.
+---
+---@param f fun(...): any|nil: the function that might throw an error
+---@param ... any|nil: args to pass to f
+---@return ErrorMessage|nil: a parsed error message, if there was an error, nil otherwise
+function OnErr.getmsg(f, ...)
+  local ok, res = pcall(f, ...)
+
+  if not ok then
+    ---@diagnostic disable-next-line: param-type-mismatch
+    return Error.parse(res)
+  end
 end
 
 return OnErr
